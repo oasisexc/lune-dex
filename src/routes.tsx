82d7d231ba54f9d415f5ce89b2483a4f4e09bf1d
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2022-06-17 13:00:48 +0900

    modify: unuse component delete

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,7 @@
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import TradePage from './pages/TradePage';
-import OpenOrdersPage from './pages/OpenOrdersPage';
 import React from 'react';
-import BalancesPage from './pages/BalancesPage';
-import ConvertPage from './pages/ConvertPage';
 import BasicLayout from './components/BasicLayout';
-import ListNewMarketPage from './pages/ListNewMarketPage';
-import NewPoolPage from './pages/pools/NewPoolPage';
-import PoolPage from './pages/pools/PoolPage';
-import PoolListPage from './pages/pools/PoolListPage';
 import { getTradePageUrl } from './utils/markets';
 
 export function Routes() {
