7ab775d05c73738832ffcc7025588429689ebd4b
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2022-06-20 10:44:09 +0900

    delete: footer info delete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,57 +28,7 @@ export const CustomFooter = () => {
       }}
     >
       <Row>
-        <Link
-          external
-          to={helpUrls.developerResources}
-          style={{ color: '#b1bac3', fontWeight: 'bold' }}
-        >
-          {'Serum Developer Resources'}
-        </Link>
-      </Row>
-      <Row>
-        <Col>
-          <Link
-            external
-            to={helpUrls.discord}
-            style={{ color: '#b1bac3', fontWeight: 'bold' }}
-          >
-            {'Discord'}
-          </Link>
-        </Col>
-        <Col style={{ margin: '0 5px', color: '#b1bac3', fontWeight: 'bold' }}>
-          {'|'}
-        </Col>
-        <Col>
-          <Link
-            external
-            to={helpUrls.telegram}
-            style={{ color: '#b1bac3', fontWeight: 'bold' }}
-          >
-            {'Telegram'}
-          </Link>
-        </Col>
-        <Col style={{ margin: '0 5px', color: '#b1bac3', fontWeight: 'bold' }}>
-          {'|'}
-        </Col>
-        <Col>
-          <Link
-            external
-            to={helpUrls.github}
-            style={{ color: '#b1bac3', fontWeight: 'bold' }}
-          >
-            {'GitHub'}
-          </Link>
-        </Col>
-      </Row>
-      <Row>{'Project Serum'}</Row>
-      <Row>
-        <Col span={12} style={{ textAlign: 'left' }}>
-          <Link external to={helpUrls.solanaBeach} style={{ color: '#b1bac3' }}>
-            {'Solana network'}
-          </Link>
-        </Col>
-        <Col span={12} style={{ textAlign: 'right', color: '#b1bac3' }}>
+        <Col span={24} style={{ textAlign: 'right', color: '#b1bac3' }}>
           {'Copyright © lunedex, Inc. All rights reserved'}
         </Col>
       </Row>
